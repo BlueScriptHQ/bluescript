b87b7bec01ac88d0a43eb626732ce06284488aae
Gerruuuth <gerrit_kreeft@hotmail.com>
2016-10-04 09:09:33 +0200

    viewHandler geupdate
    Toegevoegd aan Bluescript

---
+++ php/viewHandler.php
@@ -0,0 +1,30 @@
+<?php
+  error_reporting(-1);
+
+  try {
+     $user = "gerrizo42_admin";
+     $pass = "gsijberen17";
+     $conn = new PDO('mysql:host=localhost;dbname=gerrizo42_bluescript', $user, $pass);
+   } catch (PDOException $e) {
+     echo "Er is een probleem met de verbinding, probeer het later opnieuw.";
+     die();
+  }
+
+  if(isset($_GET["fileName"])){
+
+    $stmt = $conn->prepare("SELECT count(*) AS totaal FROM pages WHERE pages_name = :filename");
+    $stmt->execute(array(":filename" => $_GET["fileName"]));
+    $ftch = $stmt->fetch();
+    $count = $ftch["totaal"];
+
+    if($count == "0"){
+      $stmt = $conn->prepare("INSERT INTO pages (pages_name, pages_count)
+      VALUES (:name, :count)");
+      $stmt->execute(array(":name" => $_GET["fileName"], ":count" => 1));
+    } else {
+      $stmt = $conn->prepare("UPDATE pages SET pages_count = pages_count + 1 WHERE pages_name = :fileName");
+      $stmt->execute(array(":fileName" => $_GET["fileName"]));
+    }
+    return false;
+  }
+?>
